69dd30277b6c2f683dcd77c1637e570c74b71ec5
ivanquintanag <152566024+ivanquintanag@users.noreply.github.com>
2023-12-03 15:50:57 -0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://542755de-144c-4c7a-a0ee-f7a01353b123.serverhub.tripleten-services.com" # Inserta tu dirección de URL sin la barra diagonal al final
+CREATE_USER_PATH = "/api/v1/users" # Almacena la ruta para crear un usuario o usuaria en esta variable
+CREATE_KIT = "/api/v1/kits" # Almacena la ruta para crear un kit en esta variable
+DOC_PATH = '/docs/'
